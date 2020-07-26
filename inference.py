11949c2db7974288c86646d43f02612c7e4a6ac3
Kisung Kim <guitaristjimmyk@gmail.com>
2020-07-26 23:58:26 +0900

    Final Codes

--- inference.py
+++ inference.py
@@ -1,219 +1,57 @@
-# import fire
-import tqdm
-import pandas as pd
+from arena_util import load_json, write_json
+from tensorflow.keras import models
+from scipy.spatial.distance import cosine
+from tqdm import tqdm
 import numpy as np
-import multiprocessing
-from collections import Counter, defaultdict
-from arena_util import load_json
-from arena_util import write_json
+from collections import Counter
 
-import time
 
 class Inference:
-
     def _load_meta(self):
-        print('load song meta data')
-        self.song_meta = load_json('./arena_data/meta/song_meta_with_tags.json')
-        print('load song relation map')
-        self.song_map = load_json('./arena_data/meta/song_map_test.json')
-        print('load tag relation map')
-        self.tag_map = load_json('./arena_data/meta/tag_map.json')
-        print('load tag dict')
-        self.tag_dict = load_json('./arena_data/meta/tag_dict.json')
-        print('load kmeans group table')
-        self.kmeans = pd.read_pickle('./arena_data/meta/kmeans.pkl')
-        self.kmeans.index = self.kmeans.index.map(int)
-
-    def _load_val(self):
-        self.val = load_json('./arena_data/questions/val.json')
-
-    def _relation_probability(self, song_probability, vs):
-        print('test ok', vs)
-        song_probability[vs] = -1e9
-        if vs in self.song_map:
-            df = pd.DataFrame(self.song_map[vs])
-            df.set_index(keys=0, inplace=True)
-        g_id = self.kmeans.loc[vs, 'group']
-        return df, g_id
-
-    def _colaborate_probability(self, manager, vs_list):
-
-        relation_songs = pd.DataFrame()
-
-        for vs in vs_list:
-            manager[vs] = -1e9
-
-            e = False
-            try:
-                sm = self.song_map[str(vs)]
-            except:
-                e = True
-
-            if not e:
-                if len(relation_songs) == 0:
-                    relation_songs = pd.DataFrame(self.song_map[str(vs)])
-                    relation_songs.set_index(keys=0, inplace=True)
-                else:
-                    df = pd.DataFrame(self.song_map[str(vs)])
-                    df.set_index(keys=0, inplace=True)
-                    relation_songs = relation_songs.add(df, fill_value=0)
-
-
-        if len(relation_songs.index) > 0:
-            relation_songs.fillna(value=0, inplace=True)
-            for i in relation_songs.index:
-                try:
-                    manager[i] += float(relation_songs.loc[i])
-                except IndexError:
-                    manager[i] = float(relation_songs.loc[i])
-
-    def _group_probability(self, manager, vs_list):
-        group_ids = []
-        for vs in vs_list:
-            g_id = self.kmeans.loc[vs, 'group']
-            group_ids.append(str(g_id))
-
-        group_ids_cnt = Counter(group_ids)
-        g_sum = len(group_ids)
-        group_list = [(gid, group_ids_cnt[gid]/g_sum) for gid in group_ids_cnt]
-
-        for g, p in group_list:
-            kp = p
-            for i in self.kmeans[self.kmeans['group'] == int(g)].index:
-                try:
-                    manager[i] += p
-                except IndexError:
-                    manager[i] = p
-
-    def _from_tag_probability(self, manager, vt_list):
-        if len(vt_list) != 0:
-            tp = 1/len(vt_list)
-            for tag in vt_list:
-                if tag in self.tag_dict:
-                    for i in self.tag_dict[tag]:
-                        try:
-                            manager[i] += tp
-                        except IndexError:
-                            manager[i] = tp
-
-    def _predict_songlist_multi(self, vt_list, vs_list):
-        p_manager = multiprocessing.Manager()
-        song_probability = p_manager.dict()
-
-        cp = multiprocessing.Process(target=self._colaborate_probability, args=(song_probability, vs_list))
-        gp = multiprocessing.Process(target=self._group_probability, args=(song_probability, vs_list))
-        tp = multiprocessing.Process(target=self._from_tag_probability, args=(song_probability, vt_list))
-
-        cp.start()
-        gp.start()
-        tp.start()
-
-        print('process start')
-
-        cp.join()
-        gp.join()
-        tp.join()
-
-        print('process join')
-
-        sp = pd.Series(song_probability).sort_values(ascending=False)
-
-        cp.close()
-        gp.close()
-        tp.close()
-
-        return list(sp.index[:100])
-
-
-    def _predict_songlist(self, vt_list, vs_list):
-        # song_probability = defaultdict(float)
-        song_probability = np.zeros(len(self.song_meta))
-        # relation_songs = pd.DataFrame()
-        # group_ids = []
-
-        for vs in vs_list:
-            song_probability[vs] = -1e9
-
-            e = False
-            try:
-                sm = self.song_map[str(vs)]
-            except:
-                e = True
-
-            if not e:
-                for s in sm:
-                    song_probability[s[0]] += s[1]
-
-            g_id = self.kmeans.loc[vs, 'group']
-            group_ids.append(str(g_id))
-
-        if len(relation_songs.index) > 0:
-            relation_songs.fillna(value=0, inplace=True)
-            for i in relation_songs.index:
-                song_probability[int(i)] += float(relation_songs.loc[i])
-
-        group_ids_cnt = Counter(group_ids)
-        g_sum = len(group_ids)
-        group_list = [(gid, group_ids_cnt[gid]/g_sum) for gid in group_ids_cnt]
-
-        for g, p in group_list:
-            # kp = p
-            for i in self.kmeans[self.kmeans['group'] == int(g)].index:
-                song_probability[int(i)] += p
-
-        if len(vt_list) != 0:
-            tp = 1/len(vt_list)
-            for tag in vt_list:
-                if tag in self.tag_dict:
-                    for i in self.tag_dict[tag]:
-                        song_probability[int(i)] += tp
-        sp = pd.Series(song_probability).sort_values(ascending=False)
-        return list(sp.index[:100])
-
-    def _predict_taglist(self, vt_list, vs_list):
-        relation_tags = pd.Series()
-        tag_probability = defaultdict(float)
-        for vt in vt_list:
-            tag_probability[vt] = -1e9
-            if vt in self.tag_map:
-                relation_tags.add(pd.Series(self.tag_map[vt]), fill_value=0)
-        if len(relation_tags) > 0:
-            relation_tags.fillna(value=0, inplace=True)
-            for i in relation_tags.index:
-                tag_probability[i] += relation_tags[i]
-
-        tags = []
-        for vs in vs_list:
-            if 'tags' in self.song_meta[vs]:
-                tags += self.song_meta[vs]['tags']
-        tag_cnt = Counter(tags)
-        _sum = sum(tag_cnt.values())
-        for tag in tag_cnt:
-            tag_probability[tag] += tag_cnt[tag]/_sum
-        tag_probability = pd.Series(tag_probability).sort_values(ascending=False)
-        return list(tag_probability.index)[:10]
+        self.tag_list = load_json('./arena_data/meta/AE_tag_list.json')
+        self.test = load_json('./test.json')
 
     def run(self):
-        print('load meta data')
+        print('loading meta data...')
         self._load_meta()
 
-        print('load val data')
-        self._load_val()
-
-        print('predict song list and tag list')
+        model = models.load_model('./models/auto_encoder.h5')
         result = []
-        for v in tqdm.tqdm(self.val):
-            predict = dict()
-            predict['id'] = v['id']
-            predict['songs'] = self._predict_songlist(v['tags'], v['songs'])
-            predict['tags'] = self._predict_taglist(v['tags'], predict['songs'])
-            result.append(predict)
 
-        print('write result file')
+        for t in tqdm(self.test):
+            input_song_vec = np.zeros((1, 707989))
+            input_tag_vec = np.zeros((1, len(self.tag_list)))
+            predict = dict()
+            predict['id'] = t['id']
+            for song in t['songs']:
+                input_song_vec[0][song] = 1
+            for tag in t['tags']:
+                if tag in self.tag_list:
+                    input_tag_vec[0][self.tag_list.index(tag)] = 1
+            song_vec, tag_vec = model([input_song_vec, input_tag_vec])
+
+            song_vec = np.array(song_vec[0])
+            tag_vec = np.array(tag_vec[0])
+            song_rank = song_vec.argsort()
+            tag_rank = tag_vec.argsort()
+            pred_songs = []
+            pred_tags = []
+            i = -1
+            while len(pred_songs) < 100:
+                if song_rank[i] not in t['songs']:
+                    pred_songs.append(song_rank[i])
+                i -= 1
+            i = -1
+            while len(pred_tags) < 10:
+                if self.tag_list[tag_rank[i]] not in t['tags']:
+                    pred_tags.append(self.tag_list[tag_rank[i]])
+                i -= 1
+            predict['songs'] = pred_songs
+            predict['tags'] = pred_tags
+            result.append(predict)
         write_json(result, 'result.json')
 
 
 if __name__ == '__main__':
-    # fire.Fire()
-    inference = Inference()
-    inference.run()
+    ifrc = Inference()
+    ifrc.run()
--- train.py
+++ train.py
@@ -1,51 +1,150 @@
-import json
-import pandas as pd
+from arena_util import load_json, write_json
+from tensorflow.keras import layers, models, metrics, optimizers, regularizers
+from tqdm import tqdm
+import random
+import numpy as np
+from multiprocessing import Pool, cpu_count
 
 
-def predict_tags(v_tags, song_list, meta):
-    t = []
-    for s in song_list:
-        if 'tags' in meta[s]:
-            t += meta[s]['tags']
-    tags = list(pd.Series(t).value_counts(ascending=False).index)
-    for rm in v_tags:
-        if rm in tags:
-            tags.remove(rm)
-    return tags[:10]
+class AutoEncoder:
+    def _load_train(self):
+        data = load_json('./arena_data/orig/train.json')
+        self.train = []
+        self.song_list = set()
+        self.tag_list = set()
+        print('train data filtering...')
+        for t in tqdm(data):
+            if t['like_cnt'] > 50:
+                self.train.append(t)
+                self.song_list.update(t['songs'])
+                self.tag_list.update(t['tags'])
+        self.song_list = list(self.song_list)
+        self.tag_list = list(self.tag_list)
+        self.total_song_num = 707989
 
+        write_json(self.tag_list, 'meta/AE_tag_list.json')
 
-def predict_songs(song_list):
-    df = pd.read_pickle('cos_sim.pkl')
-    group_list = []
-    for s in song_list:
-        group_list.append(df.loc[s, 'group_idx'])
-    group_list = list(pd.Series(t).value_counts(ascending=True).index)
-    result = []
-    while len(result) < 100:
-        g_idx = group_list.pop()
-        result += list(df[df['group_idx'] == g_idx].index)
-        if len(group_list) == 0:
-            break
-    return result[:100]
+    def _build_model(self):
+        input_songs = layers.Input(shape=(self.total_song_num,), name='in_songs')
+        input_tags = layers.Input(shape=(len(self.tag_list),), name='in_tags')
+
+        song_dense = layers.Dense(32, kernel_regularizer=regularizers.l2(0.0001))(input_songs)
+        batch_norm = layers.BatchNormalization()(song_dense)
+        song_dense = layers.Activation(activation='relu')(batch_norm)
+
+        tag_dense = layers.Dense(32, kernel_regularizer=regularizers.l2(0.0001))(input_tags)
+        batch_norm = layers.BatchNormalization()(tag_dense)
+        tag_dense = layers.Activation(activation='relu')(batch_norm)
+
+        input_song_tag = layers.Concatenate(axis=1)([song_dense, tag_dense])
+
+        dense = layers.Dense(1024, kernel_regularizer=regularizers.l2(0.0001))(input_song_tag)
+        batch_norm = layers.BatchNormalization()(dense)
+        dense = layers.Activation(activation='relu')(batch_norm)
+
+        song_dense = layers.Dense(64, kernel_regularizer=regularizers.l2(0.0001))(dense)
+        batch_norm = layers.BatchNormalization()(song_dense)
+        song_dense = layers.Activation(activation='relu')(batch_norm)
+
+        tag_dense = layers.Dense(64, kernel_regularizer=regularizers.l2(0.0001))(dense)
+        batch_norm = layers.BatchNormalization()(tag_dense)
+        tag_dense = layers.Activation(activation='relu')(batch_norm)
+
+        output_songs = layers.Dense(self.total_song_num, activation='sigmoid', name='out_songs')(song_dense)
+        output_tags = layers.Dense(len(self.tag_list), activation='sigmoid', name='out_tags')(tag_dense)
+
+        model = models.Model(inputs=[input_songs, input_tags], outputs=[output_songs, output_tags])
+
+        model.compile(optimizer=optimizers.Adam(learning_rate=1e-3), loss='binary_crossentropy', metrics=[metrics.RootMeanSquaredError()])
+
+        return model
+
+    def _generate_batch(self, batch_size):
+        # y_songs = np.zeros((batch_size, self.total_song_num))
+        # y_tags = np.zeros((batch_size, len(self.tag_list)))
+        while True:
+            x_songs = np.zeros((batch_size, self.total_song_num))
+            x_tags = np.zeros((batch_size, len(self.tag_list)))
+            y_songs = np.zeros((batch_size, self.total_song_num))
+            y_tags = np.zeros((batch_size, len(self.tag_list)))
+            # y = np.zeros((batch_size, 740000))
+            for idx, t in enumerate(random.sample(self.train, batch_size)):
+                # y_song_vec = np.zeros(self.total_song_num)
+                # y_tag_vec = np.zeros(len(self.tag_list))
+                # song_vec = np.zeros(707989)
+                for i, song in enumerate(t['songs']):
+                    if i%2 == 0:
+                        x_songs[idx, song] = 1
+                    # y_song_vec[song] = 1
+                    y_songs[idx, song] = 1
+                # tag_vec = np.zeros(len(self.tag_list))
+                for i, tag in enumerate(t['tags']):
+                    if i%2 == 0:
+                        x_tags[idx, self.tag_list.index(tag)] = 1
+                    # y_tag_vec[self.tag_list.index(tag)] = 1
+                    y_tags[idx, self.tag_list.index(tag)] = 1
+                # x_songs[idx, :] = song_vec
+                # x_tags[idx, :] = tag_vec
+                # y_songs[idx, :] = y_song_vec
+                # y_tags[idx, :] = y_tag_vec
+            yield {'in_songs': x_songs, 'in_tags': x_tags}, {'out_songs': y_songs, 'out_tags': y_tags}
+
+    def run(self):
+        self._load_train()
+        model = self._build_model()
+        # model = models.load_model('./models/auto_encoder.h5')
+        print(model.summary())
+        model.fit_generator(self._generate_batch(64), steps_per_epoch=len(self.train)//64, epochs=5)
+
+        model.save('./models/auto_encoder.h5')
+
+    def val_inference(self):
+        model = models.load_model('./models/auto_encoder.h5')
+        val = load_json('./arena_data/questions/val.json')
+        tag_list = load_json('./arena_data/meta/AE_tag_list.json')
+        result = []
+
+        for v in tqdm(val):
+            input_song_vec = np.zeros((1, 707989))
+            input_tag_vec = np.zeros((1, len(tag_list)))
+            predict = dict()
+            predict['id'] = v['id']
+            for s in v['songs']:
+                input_song_vec[0][s] = 1
+            for t in v['tags']:
+                if t in tag_list:
+                    input_tag_vec[0][tag_list.index(t)] = 1
+            song_vec, tag_vec = model([input_song_vec, input_tag_vec])
+
+            song_vec = np.array(song_vec[0])
+            tag_vec = np.array(tag_vec[0])
+            song_rank = song_vec.argsort()
+            tag_rank = tag_vec.argsort()
+            pred_songs = []
+            pred_tags = []
+            i = -1
+            while len(pred_songs) < 100:
+                if song_rank[i] not in v['songs']:
+                    pred_songs.append(song_rank[i])
+                i -= 1
+            i = -1
+            while len(pred_tags) < 10:
+                if tag_list[tag_rank[i]] not in v['tags']:
+                    pred_tags.append(tag_list[tag_rank[i]])
+                i -= 1
+            predict['songs'] = pred_songs
+            predict['tags'] = pred_tags
+            result.append(predict)
+        write_json(result, 'AE_results.json')
 
 
 if __name__ == '__main__':
-    print('load song meta data')
-    with open('song_meta_with_tags.json', 'r', encoding='utf-8') as f:
-        song_meta = json.load(f)
-
-    print('load target data')
-    with open('val.json', 'r', encoding='utf-8') as f:
-        val = json.load(f)
-
-    result = []
-    pred = {}
-    for v in val[:5]:
-        pred['id'] = v['id']
-        pred['songs'] = predict_songs(song_list=v['songs'])
-        pred['tags'] = predict_tags(v_tags=v['tags'], song_list=v['songs'], meta=song_meta)
-        result.append(pred)
-
-    print('result file writing...')
-    with open('result.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, ensure_ascii=False, indent='\t')
+    ae = AutoEncoder()
+    ae.run()
+    while True:
+        val_flag = input('validation inference start? : (yes|no)')
+        if val_flag == 'yes':
+            ae.val_inference()
+        elif val_flag == 'no':
+            print('exit process')
+            break
